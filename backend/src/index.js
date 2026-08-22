7c9beb5f71a34481cd4dad4a6de80deed64002cf
piyushtiwari30 <tiwaripiyush533@gmail.com>
2026-08-23 01:24:34 +0530

    Add Dockerfile and .dockerignore for multi-stage build setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,20 @@ import { connectDb } from "./lib/db.js";
 import { clerkMiddleware } from '@clerk/express';
 import cors from "cors";
 
+import fs from "fs";
+import path from "path"
+
 
 const app=express();
 const PORT=process.env.PORT;
 const FRONTEND_URL=process.env.FRONTEND_URL;
+
+
+const publicDir =path.join(process.cwd(),"public");
+
+
+
+
 app.use(express.json());// this allow as to parse the incomming data ,parse the json data comming form the client side
 app.use(cors({origin:FRONTEND_URL,credentials:TRUE}))//ORIGIN WITH URL WILL MAKE THE PRETICULAR URL TO ACCES THE DATABBASE HERE ITS "FRONTEND_URL"
 //AND credentials will make the client to send the cookies or auth header with the request.
@@ -20,6 +30,18 @@ app.get("/health",(req,res)=>{
     // req.auth ---->basically middleware is being used to check for the authentication for the reqest and then serve the responce
     res.status(200).json({ok:true});
 });
+
+
+//if the public directory exists, serve the static file
+//this if for the production build
+if (fs.existSync(publicDir)){
+
+    app.use(express.static(publicDir));
+    app.get("/{*any}",(req,res,next)=>{
+        res.sendFile(path.join(publicDir,"index.html"),(err)=>next(err));
+    });
+}
+
 app.listen(PORT,()=> {
     connectDb();
     console.log("server is up and running on port 3000",PORT)
