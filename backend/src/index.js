7b9b806a43c7b7a9a3be4130e9ba65bb2b27cf33
piyushtiwari30 <tiwaripiyush533@gmail.com>
2026-08-27 00:52:39 +0530

    fix: update import statement to include app from socket.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ import clerkWebhook from "./webhooks/clerk.webhook.js"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-import { server } from "./lib/socket.js";
+import { app,server } from "./lib/socket.js";
 
 
 
