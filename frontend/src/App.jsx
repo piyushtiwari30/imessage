7a52766f6643f0f59cca11142d7ce5c64fdbf3ee
piyushtiwari30 <tiwaripiyush533@gmail.com>
2026-08-26 23:30:09 +0530

    fix: update import statement for PageLoader component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import {Routes,Route, Navigate} from "react-router"
 import ChatPage from './pages/ChatPage.jsx';
 import AuthPage from './pages/AuthPage.jsx';
 import {useAuth} from "@clerk/react"
-import PageLoader from './components/PageLoader.jsx';
+import {PageLoader} from './components/PageLoader.jsx';
 
 function App() {
   const {isSignedIn,isLoaded}= useAuth();
